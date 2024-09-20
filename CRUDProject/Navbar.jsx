4a398c743e940063f7db9bb59d50cb331548cf91
Isika Maiti <it2027@cemk.ac.in>
2024-09-20 18:33:39 +0530

    Add files via upload

---
+++ CRUDProject/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {AppBar,Toolbar,Typography,styled} from '@mui/material';
+import { NavLink } from 'react-router-dom';
+const Header= styled(AppBar)`
+    background: #111111;
+`
+const Typo=styled(NavLink)`
+    font-size: 20px;
+    margin-right: 20px;
+    text-decoration:none;
+    color:inherit;
+`
+const Navbar = () => {
+  return (
+    <div>
+      <Header position='static'>
+            <Toolbar>
+                <Typo to="/">Code for Interview</Typo>
+                <Typo to="/all">All User</Typo>
+                <Typo to="/add">Add User</Typo>
+            </Toolbar>
+      </Header>
+    </div>
+  )
+}
+
+export default Navbar
